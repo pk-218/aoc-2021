2728c1dadf44ea05795434b7ccfc6d15afae18ec
pk-218 <2182001pk@gmail.com>
2021-12-02 19:58:09 +0530

    ✔ Day02 challenge completed

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -1,6 +1,41 @@
 package day02
 
 class Day02 {
-    fun part1() { }
-    fun part2() { }
+    fun part1(input: List<String>): Int {
+        val n = input.size
+        var horizontalDistance = 0
+        var depth = 0
+
+        for (i in 0 until n) {
+            val str = input[i].split(" ")
+            val x = str[1].toInt()
+            when (str[0]) {
+                "forward" -> horizontalDistance += x
+                "up" -> depth -= x
+                "down" -> depth += x
+            }
+        }
+        return horizontalDistance * depth
+    }
+
+    fun part2(input: List<String>): Int {
+        val n = input.size
+        var horizontalDistance = 0
+        var depth = 0
+        var aim = 0
+
+        for (i in 0 until n) {
+            val str = input[i].split(" ")
+            val x = str[1].toInt()
+            when (str[0]) {
+                "forward" -> {
+                    horizontalDistance += x
+                    depth += aim * x
+                }
+                "up" -> aim -= x
+                "down" -> aim += x
+            }
+        }
+        return horizontalDistance * depth
+    }
 }
--- src/day02/Day02Test.kt
+++ src/day02/Day02Test.kt
@@ -1,16 +1,23 @@
 package day02
 
+import day01.Day01
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import readInput
 
 internal class Day02Test {
 
+    private val day02 = Day02()
+    private val testInput = readInput("Day02_test")
+
     @Test
-    fun part1() {
+    fun part1Test() {
+        assertEquals(150, day02.part1(testInput))
     }
 
     @Test
-    fun part2() {
+    fun part2Test() {
+        assertEquals(900, day02.part2(testInput))
     }
 }
